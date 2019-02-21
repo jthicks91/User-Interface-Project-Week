ce4e23f9a2cba3766d80fa3f8dea966d1814e74d
jthicks91 <jordanthicks91@gmail.com>
2019-02-21 11:41:20 -0800

    fixed bug that caused nav expansion eventListener to activate upon clicking anywhere on the home and services pages. Working only when clicking on menu for respective pages.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // JS goes here
+
+//nav expand/collapse logic below
+
+class Menu {
+  constructor(element) {
+    this.element = element;
+    this.menu = this.element.querySelector("#menu");
+    this.toggle = this.element.querySelector("#toggle");
+    this.toggle.addEventListener("click", () => {
+      this.toggleMenu();
+    });
+  }
+  Expanded() {
+    return this.element.classList.contains("expanded");
+  }
+  toggleMenu() {
+    if (this.Expanded()) {
+      this.collapse();
+    } else {
+      this.expand();
+    }
+  }
+  collapse() {
+    this.element.classList.add("dropdown");
+    this.element.classList.remove("expanded");
+  }
+  expand() {
+    this.element.classList.add("expanded");
+    this.element.classList.remove("dropdown");
+  }
+}
+
+let menu = document.querySelector("nav");
+menu = new Menu(menu);
